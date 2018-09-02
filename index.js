2b6b71a297255529a382fe43a84a9d0c44b7af55
hsigge <hanna.sigge@gmail.com>
2018-09-02 15:30:57 +0200

    JavaScript sandbox files

---
+++ index.js
@@ -0,0 +1,9 @@
+let firstName = "Sophie"
+let lastName = "Sjöberg";
+
+const fullName = `${firstName} ${lastName}`;
+
+console.log(firstName + ’ ’ + lastName); 
+//outputs ”Sophie Sjöberg”
+
+console.log(fullName);
