42a76dc0ce8bbab6f5ef68a2329c5a17a46b3c1d
hsigge <hanna.sigge@gmail.com>
2018-09-02 16:33:53 +0200

    add array and object list

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
-let firstName = "Sophie"
+let firstName = "Sophie";
 let lastName = "Sjöberg";
 
 const fullName = `${firstName} ${lastName}`;
 
-console.log(firstName + ’ ’ + lastName); 
+console.log(firstName + ` ` + lastName); 
 //outputs ”Sophie Sjöberg”
 
 console.log(fullName);
+
+//Arrays
+let strongWomen = ['Diana', 'Theresa', 'Madonna'];
+
+console.log(strongWomen);
+console.log(strongWomen.lenght); //gives no of items in array
+console.log(strongWomen)[1]; //gives us ”Theresa”
+
+//Objects (arrays with many values i.e. like a hash)
+console.log(bil.color);
+
+let car = {
+    brand: 'Volvo', //comma to separate
+    wheels: 4,
+    color: 'red'
+}; //close with semicolon at the end
+
+console.log(bil.color);
