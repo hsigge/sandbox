5e327fe06fa990dd75af43863caaf454c9a432fe
hsigge <hanna.sigge@gmail.com>
2018-09-02 18:27:43 +0200

    add answers to first 4 CA questions.

--- index.js
+++ index.js
@@ -13,15 +13,27 @@ let strongWomen = ['Diana', 'Theresa', 'Madonna'];
 
 console.log(strongWomen);
 console.log(strongWomen.lenght); //gives no of items in array
-console.log(strongWomen)[1]; //gives us ”Theresa”
+console.log(strongWomen[1]); //gives us ”Theresa”
 
 //Objects (arrays with many values i.e. like a hash)
-console.log(bil.color);
-
 let car = {
     brand: 'Volvo', //comma to separate
     wheels: 4,
     color: 'red'
 }; //close with semicolon at the end
 
-console.log(bil.color);
+console.log(car.color);
+
+//objects within objects
+let macchina = {
+    brand: 'fiat',
+    wheels: 3,
+    color: {
+        color1: 'red',
+        color2: 'black'
+    },
+};
+
+console.log(macchina.color.color2);
+
+
